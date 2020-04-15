0ecdb5b7e2122de4343a27a7cd65852aa2bf2f47
weblogistics <59546261+weblogistics@users.noreply.github.com>
2020-04-15 14:56:20 +1200

    Correct typos...

--- setup/start.sh
+++ setup/start.sh
@@ -98,7 +98,7 @@ EOF
 # Ensure the Hosts file is correct
 cat > /etc/hosts << EOF
 127.0.0.1    localhost
-${PUBLIC_IP}   $(PRIMARY_HOSTNAME)
+${PUBLIC_IP}   ${PRIMARY_HOSTNAME}
 # The following lines are desirable for IPv6 capable hosts
 ::1          localhost ip6-localhost ip6-loopback
 EOF
